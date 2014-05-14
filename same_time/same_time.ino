3c4339065ce1b88b3bb7b53d7f59debc1cc9e2e2
7imbrook <timbrook480@gmail.com>
2014-05-14 08:50:00 -0400

    Updated to use interrupts for same time trigger

--- same_time/same_time.ino
+++ same_time/same_time.ino
@@ -1,31 +1,49 @@
+int inputPin = 7;
+int startPin = 22;
+int endPin = 45;
+volatile bool fire = false;
 
-int buttonState;
+void setup()
+{
+  // Interrupts
+  attachInterrupt(inputPin, triggerCam, RISING);
 
-void setup() {
-  
-  // Input
-  pinMode(7, INPUT);
-  
   // set the digital pin as output:
-  for(int i = 22; i < 45; i++) {
+  for(int i = startPin; i < endPin; i++) {
     pinMode(i, OUTPUT);
     digitalWrite(i, LOW);
   }
 }
 
+// 22-29 PORTA (0x01-0x80)
+// 30-37 PORTC (0x80-0x01)
+// 38-45 PORTD (0x80-0x01)
+
 void loop()
 {
-  buttonState = digitalRead(7); 
-  if (buttonState == HIGH) {
-    PORTA = B11111111; // T1i
-    delay(1);
-    PORTC = B11111111; // T3i
-    delay(13); // Deplay behind T5i for T3i
-    PORTD = B11111111; // T5i
+  if (state) {
+    // Triggering output is delayed by 14 milliseconds due to the delay time it
+    // takes the cameras to respond to the trigger sent to them. The different
+    // models take different times and this is accounted for.
+    PORTA = B11111111; // T1i fire here
+    delay(1);  // Delay behing T3i for T1i
+    PORTC = B11111111; // T3i fire here
+    delay(13); // Delay behind T5i for T3i
+    PORTD = B11111111; // T5i fire here
+
+    // Wait and reset output back to low
     delay(20);    
     PORTA = B00000000;
     PORTC = B00000000;
     PORTD = B00000000;
+
+    // One second reset time
+    delay(1000);
+    state = false;
   }
 }
 
+void triggerCam()
+{
+  state = true;
+}
